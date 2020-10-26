206ee83dbc2496c50c443830d4d09a9fdffbdc7f
daniellespell21 <71269685+daniellespell21@users.noreply.github.com>
2020-10-25 19:26:17 -0500

    Add files via upload

---
+++ Fixit9ControlsSpell.appstudio/cache/mobileNav.cache
@@ -0,0 +1,20 @@
+
+hmbMenu.onclick=function(s){ // when just click the control. 's' is
+                              // the object returned
+    if (typeof(s) == "object") { // do nothing - they just clicked on the control
+       return
+    } else {
+       switch(s) {
+            case "Dessert Voting":
+                ChangeForm(dessertVoting)
+                break
+            case "Describe Me":
+                ChangeForm(describeYou)
+                break
+            case "Favorite Exercises":
+               ChangeForm(favExercises)
+               console.log(s)
+               break
+       }  //switch
+   } //else
+}
---
+++ Fixit9ControlsSpell.appstudio/forms/dessertVoting/dessertVoting.js
@@ -0,0 +1,29 @@
+var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
+                  'Keylime Pie','Cherry Cobbler','Chocolate Chip Cookies']
+
+dessertVoting.onshow=function(){
+    drpDesserts.clear()   
+  /* add a loop here that adds all the desserts in the array 
+     to the dropdown.
+  */
+  for (i = 0; i<= dessertList.length-1;i++){
+    drpDesserts.addItem(dessertList[i])}
+}
+
+
+drpDesserts.onclick=function(){
+   // this 'if' kicks user out if they  just clicked on control 
+ // but not on one item in the list.
+    if (typeof(s) == "object")
+      return                    
+    else {  // the user picked something
+        /* add code here to get item user chose and output 
+        it in a label, using a literal.
+        */
+        lblDessert.value = `You picked ${drpDesserts.selection} -that is a great choice!`
+    }
+}
+
+btn1.onclick=function(){
+  ChangeForm(describeYou)
+}
